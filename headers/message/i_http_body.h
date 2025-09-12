cc8b037d5abe361db294efd7a44dc6164acd0bd1
IvanPatrenkov <ivanestver24@gmail.com>
2025-09-13 00:07:07 +0300

    add: define functions in IHttpBody

--- headers/message/i_http_body.h
+++ headers/message/i_http_body.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "core/iserializable.h"
+#include <istream>
 
 class IHTTPBody : public ISerializable, public IDeserializable
 {
 public:
 	virtual ~IHTTPBody() = default;
 
-	virtual std::vector<char> GetRawData() const = 0;
-	virtual void SetRawData(const std::vector<char>& rawData) = 0;
+	virtual std::vector<char> ToBytes() const = 0;
+	virtual String ToString() const = 0;
+	virtual std::unique_ptr<std::istream> ToStream() const = 0;
+	virtual size_t GetSize() const = 0;
+	virtual bool IsEmpty() const = 0;
+	virtual String GetContentType() const = 0;
 };
