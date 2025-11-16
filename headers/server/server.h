624c88eda41b5732e6ed0871c3e0609803757e17
IvanPatrenkov <ivanestver24@gmail.com>
2025-11-17 01:19:31 +0300

    add: add server class that encapsulates server logic and provides functionality to set routing

---
+++ headers/server/server.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "net_core/platform.h"
+#include "core/str.h"
+#include "message/i_http_request.h"
+#include "message/http_response.h"
+#include <functional>
+#include <unordered_map>
+
+namespace server
+{
+	class Server
+	{
+	public:
+		using Handler = std::function<void(const message::IHTTPRequest*, message::HTTPResponse&)>;
+
+	public:
+		void AddHandler(const String& path, Handler&& handler);
+		void Run(port_t port);
+
+	private:
+		std::unordered_map<String, Handler> m_handlers;
+	};
+}
