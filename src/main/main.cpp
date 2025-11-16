8f60084ecf51eeb5952abf6d8961204a2dd0933a
Ivanestver <56598851+Ivanestver@users.noreply.github.com>
2025-11-17 01:22:33 +0300

    Update to v0.3.0
    * add: function to get header keys
    
    * Update to v0.1.4
    
    * add: an http response representation class and an example in main.cpp
    
    * add: add hash for String
    
    * Update to v0.2.1
    
    * add: add server class that encapsulates server logic and provides functionality to set routing
    
    * Update to v0.3.0

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,79 +1,27 @@
-﻿#include "net_core/net_funcs.h"
-#include <iostream>
-#include "parser/request_parser.h"
-#include "message/http_response.h"
+﻿#include <iostream>
+#include "server/server.h"
 
 int main()
 {
 	port_t port = 8088;
-	String errorMessage = net_core::Startup();
-	if (!errorMessage.is_empty())
-	{
-		std::cout << errorMessage << std::endl;
-		return 1;
-	}
+	server::Server server;
 
-	auto [sock, err] = net_core::CreateSocket(AddressFamily::INET, SockType::STREAM);
-	if (!err.is_empty())
-	{
-		std::cout << err << std::endl;
-		return 1;
-	}
-
-	sockaddr_in_t addr;
-	net_core::InitSockAddrIn(&addr, port, AddressFamily::INET);
-
-	errorMessage = net_core::Bind(&addr, sock);
-	if (!errorMessage.is_empty())
-	{
-		std::cout << errorMessage << std::endl;
-		return 1;
-	}
-
-	net_core::Listen(sock, 0);
-	std::cout << "Started listening on " << port << std::endl;
-
-	while (true)
-	{
-		sockaddr_in_t clientAddr;
-		auto [clientSock, err] = net_core::Accept(sock, &clientAddr);
-		if (!err.is_empty())
+	server.AddHandler("/", [](const message::IHTTPRequest* request, message::HTTPResponse& response)
 		{
-			std::cout << err << std::endl;
-			continue;
-		}
+			response.SetStatusCode(message::StatusCode::OK);
+			response.SetHeader("Content-Type", "text/html; charset = UTF-8");
+			auto bodyWriter = response.GetBodyWriter();
+			bodyWriter.WriteLine("<!DOCTYPE html>")
+				.WriteLine("<html>")
+				.WriteLine("<head>")
+				.WriteLine("\t<title>Hello</title>")
+				.WriteLine("</head>")
+				.WriteLine("<body>")
+				.WriteLine("\t<h1>Hello, World!</h1>")
+				.WriteLine("</body>")
+				.WriteLine("</html>");
+		});
 
-		char buffer[1024] = {0};
-		int count = recv(*clientSock, buffer, sizeof(buffer) - 1, 0);
-		if (count > 0)
-			std::cout << buffer << std::endl << std::endl;
-		String s{ buffer };
-		parser::RequestParser requestParser{ s };
-		const message::IHTTPRequest* request = requestParser.Parse();
-
-		message::HTTPResponse response;
-		response.SetStatusCode(message::StatusCode::OK);
-		response.SetHeader("Content-Type", "text/html; charset = UTF-8");
-		auto bodyWriter = response.GetBodyWriter();
-		bodyWriter.WriteLine("<!DOCTYPE html>")
-			.WriteLine("<html>")
-			.WriteLine("<head>")
-			.WriteLine("\t<title>Hello</title>")
-			.WriteLine("</head>")
-			.WriteLine("<body>")
-			.WriteLine("\t<h1>Hello, World!</h1>")
-			.WriteLine("</body>")
-			.WriteLine("</html>");
-		const std::vector<uint8_t> responseRaw = response.toBytes();
-		int iSendResult = send(*clientSock, (const char*)responseRaw.data(), responseRaw.size(), 0);
-		if (iSendResult == SOCKET_ERROR)
-		{
-			std::cout << "send failed with error: %d\n" << WSAGetLastError() << std::endl;
-			continue;
-		}
-		printf("Bytes sent: %d\n", iSendResult);
-	}
-
-	net_core::ClearUp();
+	server.Run(port);
 	return 0;
 }
---
+++ src/server/server.cpp
@@ -0,0 +1,80 @@
+#include "server/server.h"
+#include "net_core/net_funcs.h"
+#include "parser/request_parser.h"
+#include "message/http_response.h"
+#include <memory>
+
+namespace server
+{
+	void Server::AddHandler(const String& path, Handler&& handler)
+	{
+		m_handlers.emplace(path, std::move(handler));
+	}
+
+	void Server::Run(port_t port)
+	{
+		String errorMessage = net_core::Startup();
+		if (!errorMessage.is_empty())
+		{
+			std::cout << errorMessage << std::endl;
+			return;
+		}
+
+		auto [sock, err] = net_core::CreateSocket(AddressFamily::INET, SockType::STREAM);
+		if (!err.is_empty())
+		{
+			std::cout << err << std::endl;
+			return;
+		}
+
+		sockaddr_in_t addr;
+		net_core::InitSockAddrIn(&addr, port, AddressFamily::INET);
+
+		errorMessage = net_core::Bind(&addr, sock);
+		if (!errorMessage.is_empty())
+		{
+			std::cout << errorMessage << std::endl;
+			return;
+		}
+
+		net_core::Listen(sock, 0);
+		std::cout << "Started listening on " << port << std::endl;
+
+		while (true)
+		{
+			sockaddr_in_t clientAddr;
+			auto [clientSock, err] = net_core::Accept(sock, &clientAddr);
+			if (!err.is_empty())
+			{
+				std::cout << err << std::endl;
+				continue;
+			}
+
+			char buffer[1024] = {0};
+			int count = recv(*clientSock, buffer, sizeof(buffer) - 1, 0);
+			if (count > 0)
+				std::cout << buffer << std::endl << std::endl;
+			String s{ buffer };
+			parser::RequestParser requestParser{ s };
+			auto request = std::unique_ptr<const message::IHTTPRequest>{ requestParser.Parse() };
+			if (!request)
+				continue;
+
+			const auto it = m_handlers.find(request->GetStartLine()->GetPath());
+			if (it == m_handlers.end())
+				continue;
+			message::HTTPResponse response;
+			it->second(request.get(), response);
+			const std::vector<uint8_t> responseRaw = response.toBytes();
+			int iSendResult = send(*clientSock, (const char*)responseRaw.data(), responseRaw.size(), 0);
+			if (iSendResult == SOCKET_ERROR)
+			{
+				std::cout << "send failed with error: %d\n" << WSAGetLastError() << std::endl;
+				continue;
+			}
+			printf("Bytes sent: %d\n", iSendResult);
+		}
+
+		net_core::ClearUp();
+	}
+}
